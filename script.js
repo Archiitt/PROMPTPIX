4966951260b44f49edc4495e5c1690a94f7fce42
ARCHIT <145273087+Archiitt@users.noreply.github.com>
2023-10-10 14:38:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+const generateForm = document.querySelector(".generate-form");
+const generateBtn = generateForm.querySelector(".generate-btn");
+const imageGallery = document.querySelector(".image-gallery");
 
+const OPENAI_API_KEY = "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"; // Your OpenAI API key here
+let isImageGenerating = false;
+
+const updateImageCard = (imgDataArray) => {
+  imgDataArray.forEach((imgObject, index) => {
+    const imgCard = imageGallery.querySelectorAll(".img-card")[index];
+    const imgElement = imgCard.querySelector("img");
+    const downloadBtn = imgCard.querySelector(".download-btn");
+    
