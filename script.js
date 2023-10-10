2f21deb963b5aee431d3b636173a62277555078a
ARCHIT <145273087+Archiitt@users.noreply.github.com>
2023-10-10 14:39:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,46 @@
+const generateForm = document.querySelector(".generate-form");
+const generateBtn = generateForm.querySelector(".generate-btn");
+const imageGallery = document.querySelector(".image-gallery");
+
+const OPENAI_API_KEY = "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"; // Your OpenAI API key here
+let isImageGenerating = false;
+
+const updateImageCard = (imgDataArray) => {
+  imgDataArray.forEach((imgObject, index) => {
+    const imgCard = imageGallery.querySelectorAll(".img-card")[index];
+    const imgElement = imgCard.querySelector("img");
+    const downloadBtn = imgCard.querySelector(".download-btn");
+    
+    // Set the image source to the AI-generated image data
+    const aiGeneratedImage = `data:image/jpeg;base64,${imgObject.b64_json}`;
+    imgElement.src = aiGeneratedImage;
+    
+    // When the image is loaded, remove the loading class and set download attributes
+    imgElement.onload = () => {
+      imgCard.classList.remove("loading");
+      downloadBtn.setAttribute("href", aiGeneratedImage);
+      downloadBtn.setAttribute("download", `${new Date().getTime()}.jpg`);
+    }
+  });
+}
+
+const generateAiImages = async (userPrompt, userImgQuantity) => {
+  try {
+    // Send a request to the OpenAI API to generate images based on user inputs
+    const response = await fetch("https://api.openai.com/v1/images/generations", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${OPENAI_API_KEY}`,
+      },
+      body: JSON.stringify({
+        prompt: userPrompt,
+        n: userImgQuantity,
+        size: "512x512",
+        response_format: "b64_json"
+      }),
+    });
+
     // Throw an error message if the API response is unsuccessful
     if(!response.ok) throw new Error("Failed to generate AI images. Make sure your API key is valid.");
 
