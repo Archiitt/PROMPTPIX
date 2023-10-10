030331f9c8c3941d2914315a279f0639352571fa
ARCHIT <145273087+Archiitt@users.noreply.github.com>
2023-10-10 14:57:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const generateForm = document.querySelector(".generate-form");
 const generateBtn = generateForm.querySelector(".generate-btn");
 const imageGallery = document.querySelector(".image-gallery");
 
-const OPENAI_API_KEY = "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"; // Your OpenAI API key here
+const OPENAI_API_KEY = ""; // Your OpenAI API key here
 let isImageGenerating = false;
 
 const updateImageCard = (imgDataArray) => {
