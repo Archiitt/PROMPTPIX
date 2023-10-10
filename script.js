648797909085c616ea88ec2e53ef259bf1e44339
ARCHIT <145273087+Archiitt@users.noreply.github.com>
2023-10-10 14:38:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
-const generateForm = document.querySelector(".generate-form");
-const generateBtn = generateForm.querySelector(".generate-btn");
-const imageGallery = document.querySelector(".image-gallery");
-
-const OPENAI_API_KEY = "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"; // Your OpenAI API key here
-let isImageGenerating = false;
-
-const updateImageCard = (imgDataArray) => {
-  imgDataArray.forEach((imgObject, index) => {
-    const imgCard = imageGallery.querySelectorAll(".img-card")[index];
-    const imgElement = imgCard.querySelector("img");
-    const downloadBtn = imgCard.querySelector(".download-btn");
+    // Set the image source to the AI-generated image data
+    const aiGeneratedImage = `data:image/jpeg;base64,${imgObject.b64_json}`;
+    imgElement.src = aiGeneratedImage;
     
+    // When the image is loaded, remove the loading class and set download attributes
+    imgElement.onload = () => {
+      imgCard.classList.remove("loading");
+      downloadBtn.setAttribute("href", aiGeneratedImage);
+      downloadBtn.setAttribute("download", `${new Date().getTime()}.jpg`);
+    }
+  });
+}
